ad9aaa4b972760e5960ec1b661e56a39f4731e30
Dmitrii <ridimonn@mail.ru>
2026-03-13 18:20:12 +0300

    init project

---
+++ Zephyr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zephyr
+//
+//  Created by Dmitriy Kalyakin on 13/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zephyr/ZephyrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZephyrApp.swift
+//  Zephyr
+//
+//  Created by Dmitriy Kalyakin on 13/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ZephyrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
